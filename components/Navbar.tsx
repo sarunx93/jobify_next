791bfb751d2d8946493c41b2de888b643aa67152
Sarun <sarun.p@pantip.com>
2024-04-20 22:31:50 +0700

    second commit

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+import { LinksDropdown } from "./LinksDropdown"
+import { UserButton } from "@clerk/nextjs"
+import ThemeToggle from "./ThemeToggle"
+export const Navbar = () => {
+  return (
+    <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
+        <div>
+            <LinksDropdown/>
+        </div>
+        <div className="flex items-center gap-x-4">
+            <ThemeToggle/>
+            <UserButton afterSignOutUrl="/"/>
+        </div>
+    </nav>
+  )
+}
---
+++ components/Sidebar.tsx
@@ -0,0 +1,33 @@
+'use client'
+import links from '@/utils/links'
+import Logo from '@/assets/logo.svg'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { usePathname } from 'next/navigation'
+
+export const Sidebar = () => {
+    const pathname = usePathname()
+    return (
+        <aside className='py-4 px-8 bg-muted h-full'>
+            <Image
+                alt='logo'
+                src={Logo}
+                className='mx-auto'
+            />
+            <div className='flex flex-col mt-20 gap-y-4'>
+                {links.map((link)=>{
+                    return <Button 
+                                asChild 
+                                key={link.href}
+                                variant={pathname === link.href ? 'default' : 'link'}
+                            >
+                        <Link href={link.href} className='flex items-center gap-x-2'>
+                            {link.icon} <span className='capitalize'>{link.label}</span>
+                        </Link>
+                    </Button>
+                })}
+            </div>
+        </aside>
+    )
+}
